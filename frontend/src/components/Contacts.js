c5bd1c4a6cb6eb74b818f2171696ec4225c01d59
Pol de Visser <p.devisser@bxter.nl>
2023-01-27 10:23:39 +0100

    Made contactCard component

--- frontend/src/components/Contacts.js
+++ frontend/src/components/Contacts.js
@@ -2,6 +2,7 @@ import { MailIcon, PhoneIcon } from "@heroicons/react/solid";
 import MailIconButton from "./atomic/MailIconButton";
 import PhoneIconButton from "./atomic/PhoneIconButton";
 import Pill from "./atomic/Pill";
+import ContactCard from "./molecular/ContactCard";
 
 const people = [
   {
@@ -26,35 +27,7 @@ export default function Contacts() {
   return (
     <ul role="list" className="grid m-4 grid-cols-1 space-y-4">
       {people.map((person) => (
-        <li
-          key={person.email}
-          className="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200"
-        >
-          <div className="w-full flex items-center justify-between p-6 space-x-6">
-            <div className="flex-1 truncate">
-              <div className="flex items-center space-x-3">
-                <h3 className="text-gray-900 text-sm font-medium truncate">
-                  {person.name}
-                </h3>
-                {person.role && <Pill role={person.role} />}
-              </div>
-              <p className="mt-1 text-gray-500 text-sm truncate">
-                {person.title}
-              </p>
-            </div>
-            <img
-              className="w-10 h-10 bg-gray-300 rounded-full flex-shrink-0"
-              src={person.imageUrl}
-              alt=""
-            />
-          </div>
-          <div>
-            <div className="-mt-px flex divide-x divide-gray-200">
-              <MailIconButton email={person.email} />
-              <PhoneIconButton phoneNumber={person.telephone} />
-            </div>
-          </div>
-        </li>
+        <ContactCard person={person} />
       ))}
     </ul>
   );
---
+++ frontend/src/components/molecular/ContactCard.js
@@ -0,0 +1,31 @@
+import Card from "../atomic/Card";
+import Avatar from "../atomic/Avatar";
+import Pill from "../atomic/Pill";
+import MailIconButton from "../atomic/MailIconButton";
+import PhoneIconButton from "../atomic/PhoneIconButton";
+
+export default function ContactCard({ person }) {
+  return (
+    <Card>
+      <li className="flex-1 divide-y divide-gray-200">
+        <div className="flex items-center justify-between p-6 space-x-6">
+          <div className="truncate">
+            <div className="flex items-center space-x-3">
+              <h3 className="text-gray-900 text-sm font-medium truncate">
+                {person.name}
+              </h3>
+              {person.role && <Pill role={person.role} />}
+            </div>
+            <p className="text-gray-500 text-sm truncate">{person.title}</p>
+          </div>
+          <Avatar size="xl" img={person.imageUrl}></Avatar>
+        </div>
+        <div className="flex divide-x divide-gray-200">
+          <MailIconButton email={person.email} />
+
+          <PhoneIconButton phoneNumber={person.telephone} />
+        </div>
+      </li>
+    </Card>
+  );
+}
