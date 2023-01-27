c51e0b5145512c14b7be87a7bd9c80e6420078f1
Pol de Visser <p.devisser@bxter.nl>
2023-01-27 10:10:34 +0100

    removed unnecessary styling elements

--- frontend/src/components/atomic/MailIconButton.js
+++ frontend/src/components/atomic/MailIconButton.js
@@ -3,10 +3,10 @@ import PropTypes from "prop-types";
 
 export default function MailIconButton(email) {
   return (
-    <div className="w-0 flex-1 flex">
+    <div className="flex-1 flex">
       <a
         href={`mailto:${email}`}
-        className="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500"
+        className="flex-1 space-x-3 inline-flex justify-center py-4 text-sm text-gray-700 font-medium rounded-bl-lg hover:text-gray-500"
       >
         <MailIcon className="w-5 h-5 text-gray-400" aria-hidden="true" />
         <span>Email</span>
