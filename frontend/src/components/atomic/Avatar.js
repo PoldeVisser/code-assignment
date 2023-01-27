dc51ed6b5764ed08743352dcc2ccc131435741cf
Pol de Visser <p.devisser@bxter.nl>
2023-01-27 10:17:48 +0100

    added avatar w/ sizes for other avatars in app

---
+++ frontend/src/components/atomic/Avatar.js
@@ -0,0 +1,28 @@
+import PropTypes from "prop-types";
+import classNames from "../../modules/classNames";
+
+export default function Avatar({ img, size }) {
+  return (
+    <img
+      className={classNames(
+        "bg-gray-300 rounded-full flex-shrink-0",
+        size === "xs" && "w-4 h-4",
+        size === "s" && "w-6 h-6",
+        size === "md" && "w-8 h-8",
+        size === "lg" && "w-10 h-10",
+        size === "xl" && "w-12 h-12"
+      )}
+      src={img}
+      alt=""
+    />
+  );
+}
+
+Avatar.defaultProps = {
+  size: "md",
+};
+
+Avatar.propTypes = {
+  img: PropTypes.string.isRequired,
+  size: PropTypes.oneOf(["xs", "s", "md", "lg", "xl"]),
+};
