575aa7325aad1407f767d56c1325fc0ad4ec8690
Pol de Visser <p.devisser@bxter.nl>
2023-01-27 16:39:46 +0100

    Added toggleble favourite star icon

--- frontend/src/components/molecular/ContactCard.js
+++ frontend/src/components/molecular/ContactCard.js
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import Card from "../atomic/Card";
 import Avatar from "../atomic/Avatar";
 import Pill from "../atomic/Pill";
 import MailIconButton from "../atomic/MailIconButton";
 import PhoneIconButton from "../atomic/PhoneIconButton";
+import { StarIcon } from "@heroicons/react/solid";
+import classNames from "../../modules/classNames";
 
 export default function ContactCard({ person }) {
+  const [isFavourite, setFavourite] = useState(false);
+
   return (
     <Card>
       <li className="flex-1 divide-y divide-gray-200">
@@ -18,7 +23,19 @@ export default function ContactCard({ person }) {
             </div>
             <p className="text-gray-500 text-sm truncate">{person.title}</p>
           </div>
-          <Avatar size="xl" img={person.imageUrl}></Avatar>
+          <div className="flex items-center space-x-3">
+            <Avatar size="xl" img={person.imageUrl} />
+            {/* TODO implement [UPDATE]/contacts/id/isFavourite call on click */}
+            <button onClick={() => setFavourite((isFavourite) => !isFavourite)}>
+              <StarIcon
+                className={classNames(
+                  "w-5 h-5",
+                  isFavourite ? "text-yellow-500" : "text-gray-400"
+                )}
+                aria-hidden="true"
+              />
+            </button>
+          </div>
         </div>
         <div className="flex divide-x divide-gray-200">
           {person.email && <MailIconButton email={person.email} />}
